168dc3ce2b2888ac3e60b51aafa4eac41c9abdf2
Ayesha <ayesha18mbstu@gmail.com>
2022-07-21 12:43:36 +0600

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// navbar scroll top start
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+// navbar scroll top end
